64b07342fcd96fc5d744dae4a2e2a4cedc0c25fa
Administrateur <thierry-hakiim.gatchuissi@efrei.net>
2025-09-17 15:51:48 +0200

    initial commit

---
+++ lab00/src/main/java/agh/ii/prinjava/lab00/lst00_01/Main.java
@@ -0,0 +1,9 @@
+package agh.ii.prinjava.lab00.lst00_01;
+
+import jdk.jshell.execution.LocalExecutionControl;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("add(1,2) = " + Calc.add(1, 2));
+    }
+}
